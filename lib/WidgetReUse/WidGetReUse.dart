ddef55619f98d25fd8ec1ca5a068e2ed536020f6
TepDe <121762225+TepDe@users.noreply.github.com>
2023-01-08 22:10:33 +0700

    Update WidGetReUse.dart

--- lib/WidgetReUse/WidGetReUse.dart
+++ lib/WidgetReUse/WidGetReUse.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:wooiproject/HomeScreen.dart';
 import 'package:wooiproject/MapScreen.dart';
+import 'package:wooiproject/WidgetReUse/Themes.dart';
 
 class WidgetReUse extends GetxController {
   topButtonLeft({function, icon}) {
@@ -21,9 +23,8 @@ class WidgetReUse extends GetxController {
               splashColor: Colors.grey,
               color: Colors.black,
               onPressed: () {
-                if(function == 'home'){
-                  Get.to(const HomeScreen());
-
+                if (function == 'home') {
+                  Get.to(HomeScreen());
                 }
               },
               icon: Icon(icon)),
@@ -31,8 +32,10 @@ class WidgetReUse extends GetxController {
       ),
     );
   }
+
   final msc = Get.put(MapScreenController());
   late GoogleMapController mapController;
+  final theme = ThemesApp();
 
   topButtonRight({function, icon}) {
     return Padding(
@@ -50,14 +53,246 @@ class WidgetReUse extends GetxController {
               splashColor: Colors.grey,
               color: Colors.black,
               onPressed: () {
-                if(function == 'crlocation'){
+                if (function == 'crlocation') {
                   msc.request();
                   msc.moveCamera();
-
-
                 }
               },
               icon: Icon(icon)),
+        ),
+      ),
+    );
+  }
+
+  topBarHomeScreen() {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Container(
+        child: Row(
+          children: [
+            IconButton(
+                iconSize: 40,
+                splashRadius: 30,
+                onPressed: () {},
+                icon: Center(
+                  child: Icon(
+                    Icons.menu,
+                    color: theme.yellow,
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  unitOneHomeScreen() {
+    return Padding(
+      padding: EdgeInsets.only(left: 18.0, top: 20, right: 18),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Good Morning',
+                style:
+                    TextStyle(color: theme.black, fontWeight: FontWeight.bold),
+              ),
+              Text('ID 003121',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: theme.grey,
+                      fontWeight: FontWeight.bold)),
+            ],
+          ),
+          Icon(
+            Icons.account_circle,
+            size: 60,
+          )
+        ],
+      ),
+    );
+  }
+
+  unitTwoHomeScreen() {
+    return Padding(
+      padding: EdgeInsets.only(left: 18.0, top: 30, right: 18),
+      child: Container(
+        width: Get.width,
+        height: 220,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 2,
+              //offset: Offset(4, 8), // Shadow position
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Flexible(
+                child: Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: unitTwoText(),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: unitTwoText(),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: theme.orange,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text(
+                              'Total Package',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  color: theme.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Text('31321',
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: theme.grey,
+                                    fontWeight: FontWeight.bold)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  unitTwoText() {
+    return Container(
+      width: Get.width,
+      decoration: BoxDecoration(
+        color: theme.orange,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Total Package',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: theme.black,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Text('31321',
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: theme.grey,
+                      fontWeight: FontWeight.bold)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  unitThreeHomeScreen({icon, lable,price, funtion}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12.0, left: 18, right: 18),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20),
+        splashColor: Colors.transparent,
+        onTap: (){
+          Get.to(MapScreen());
+        },
+        child: Container(
+
+          decoration: BoxDecoration(
+            color: theme.orange,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Icon(icon, size: 60,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 18.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(lable,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: theme.black,
+                                fontWeight: FontWeight.bold)),
+                      ),   Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(price,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: theme.black,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
