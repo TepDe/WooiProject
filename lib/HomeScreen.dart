238e07af5a7b5673a955b53fb7c6229dab3f0500
TepDe <121762225+TepDe@users.noreply.github.com>
2023-01-08 19:08:02 +0700

    Create HomeScreen.dart
    home screen

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    );
+  }
+}
+class  HomeScreenController extends GetxController{
+
+}
