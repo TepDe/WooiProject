ad600037ecffb94cc9fe0c8c065960c0d7abbdab
TepDe <121762225+TepDe@users.noreply.github.com>
2023-01-08 22:10:24 +0700

    Create Themes.dart

---
+++ lib/WidgetReUse/Themes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class ThemesApp{
+
+  Color yellow = Colors.yellow;
+  Color orange =Color(0xffffdfb4);
+  Color black = Color(0xff000000);
+  Color grey = Colors.grey;
+
+
+}
