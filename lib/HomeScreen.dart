110a8a38a54a0edd22c85f9d3fb562e286faa464
TepDe <121762225+TepDe@users.noreply.github.com>
2023-01-08 22:09:56 +0700

    Update HomeScreen.dart

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wooiproject/WidgetReUse/Themes.dart';
+import 'package:wooiproject/WidgetReUse/WidGetReUse.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final wr = WidgetReUse();
+  final theme = ThemesApp();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            wr.topBarHomeScreen(),
+            wr.unitOneHomeScreen(),
+            wr.unitTwoHomeScreen(),
+            wr.unitThreeHomeScreen(icon: Icons.directions_car, lable: 'Car',price: '2143', funtion: 'motor'),
+            wr.unitThreeHomeScreen(icon: Icons.motorcycle, lable: 'Motorcycle',price: '2143', funtion: ''),
+          ],
+        ),
       ),
     );
   }
 }
-class  HomeScreenController extends GetxController{
 
-}
+class HomeScreenController extends GetxController {}
