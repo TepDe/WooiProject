42c2f049cfd9ae4c105908490fa4b1f082efd091
TepDe <121762225+TepDe@users.noreply.github.com>
2023-02-01 21:11:52 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:wooiproject/HomeScreen.dart';
-import 'package:wooiproject/LoginScreen.dart';
-import 'package:wooiproject/MapScreen.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:wooiproject/GlobalControl/GlobalController.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:wooiproject/LoginScreen.dart';
+import 'package:wooiproject/ViewScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp( const GetMaterialApp(
+  await Firebase.initializeApp(
+  );
+  runApp(  GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final mc = Get.put(MainController());
 
   @override
   Widget build(BuildContext context) {
@@ -24,21 +31,78 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+
+      home:  mc.isSignIn.value == true ? LogInScreen() : ViewScreen(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+class MainController extends GetxController {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void onInit() async {
+    super.onInit();
+    // changeColorButton();
+    await onAlreadySignIn();
+    await request();
+    await requestUserPermissionLocation();
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return  MapScreen();
+  }
+  Future<Position> requestUserPermissionLocation() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+      print("ERROR" + error.toString());
+    });
+    return await Geolocator.getCurrentPosition();
+  }
+
+  var protect = false.obs;
+  Position? currentPosition;
+
+  request() async {
+    await Geolocator.requestPermission()
+        .then((value) {})
+        .onError((error, stackTrace) {
+      print('$error' + "" + '$stackTrace');
+      printError();
+    });
+    protect.value = true;
+    await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .then((Position position) => currentPosition = position);
+    protect.value = true;
+    update();
+  }
+
+  var isSignIn = false.obs;
+  final glb = Get.put(GlobalController());
+
+  onAlreadySignIn() async {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+        isSignIn.value = true;
+      } else {
+        // Get.to(HomeScreen());
+        print('User is currently signed in!');
+        request();
+        glb.UID.value = (user.uid).toString();
+        isSignIn.value = false;
+      }
+    });
+    update();
   }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return  HomeScreen();
+//   }
+// }
