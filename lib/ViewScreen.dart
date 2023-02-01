2131a84deb20a65ce1de9265c2486675e8fbc20f
TepDe <121762225+TepDe@users.noreply.github.com>
2023-02-01 21:12:51 +0700

    Create ViewScreen.dart

---
+++ lib/ViewScreen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:wooiproject/AccountScreen.dart';
+import 'package:wooiproject/ActivityScreen.dart';
+import 'package:wooiproject/HomeScreen.dart';
+import 'package:wooiproject/NotificationScreen.dart';
+import 'package:wooiproject/WidgetReUse/Themes.dart';
+
+
+class ViewScreen extends StatefulWidget {
+  const ViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewScreen> createState() => _ViewScreenState();
+}
+
+class _ViewScreenState extends State<ViewScreen> {
+
+  var viewScreen = [
+    HomeScreen(),
+    ActivityScreen(),
+    NotificationScreen(),
+    AccountScreen(),
+  ];
+
+  final themes = ThemesApp();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WillPopScope(
+        onWillPop: ()async=> false,
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: reUseIcon(icons: Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: reUseIcon(icons: Icons.menu),
+                label: 'Activity',
+              ),
+              BottomNavigationBarItem(
+                icon: reUseIcon(icons: Icons.notifications),
+                label: 'Notifications',
+              ),
+              BottomNavigationBarItem(
+                icon: reUseIcon(icons: Icons.account_circle),
+                label: 'Account',
+              ),
+            ],
+            unselectedFontSize: 12,
+            unselectedLabelStyle: TextStyle(color: themes.deepOrange),
+            selectedLabelStyle: TextStyle(color: themes.deepOrange,),
+            selectedItemColor: themes.deepOrange,
+            elevation: 5.0,
+            unselectedItemColor: themes.grey,
+            currentIndex: selectedIndex,
+            backgroundColor: themes.liteOrange,
+            type: BottomNavigationBarType.fixed, // Fixed
+
+            onTap: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          ),
+          body: viewScreen[selectedIndex],
+        ),
+      ),
+    );
+  }
+
+  int selectedIndex = 0;
+
+  reUseIcon({icons}) {
+    return Icon(
+      icons,
+      size: 40,
+    );
+  }
+}
