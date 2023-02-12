3eebc870f4721f4f609beced019c2406a903bffe
TepDe <121762225+TepDe@users.noreply.github.com>
2023-02-12 13:55:43 +0700

    Update MapScreen.dart

--- lib/MapScreen.dart
+++ lib/MapScreen.dart
@@ -11,209 +11,53 @@ import 'dart:math' show cos, sqrt, asin;
 
 import 'WidgetReUse/ReUseWidget.dart';
 
-// class MapScreen extends StatelessWidget {
-//   final wr = ReUseWidget();
-//   late GoogleMapController mapController;
-//   final ft = Get.put(MapScreenController());
-//   final udl = Get.put(UpDateLocation());
-//   final gsc = Get.put(GlbSuperController());
-//
-//   MapScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return
-//   }
-// }
-
 class MapScreen extends StatefulWidget {
-  MapScreen({Key? key, this.las, this.longs}) : super(key: key);
-  var las;
-  var longs;
+  MapScreen({Key? key}) : super(key: key);
 
   @override
-  State<MapScreen> createState() => _MapScreenState(las, longs);
+  State<MapScreen> createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-  var las;
-  var longs;
-
-  _MapScreenState(this.las, this.longs);
-
   late GoogleMapController mapController;
   final wr = ReUseWidget();
-
+  final Completer<GoogleMapController> _controller = Completer();
+  bool isloading = false;
   final gsc = Get.put(GlbSuperController());
-
-  @override
-  void initState() {
-    super.initState();
-    diaplayDriverDistance();
-    getLocation();
-    onDisplayDriver();
-  }
-
   late Position position;
   double latitude = 0.0;
   double longitude = 0.0;
   late LatLng currentPostion;
   late LatLng driverPostion = const LatLng(11.568553, 104.893228);
-
-  void getUserLocation() async {
-    position = await GeolocatorPlatform.instance.getCurrentPosition();
-    setState(() {
-      currentPostion = LatLng(position.latitude, position.longitude);
-      latitude = position.latitude;
-      longitude = position.longitude;
-    });
-    await onDisplayDriver();
-  }
-
-  static const LatLng showLocation =
-      LatLng(27.7089427, 85.3086209); //location to show in map
-  final Set<Marker> markerList = {};
-  Set<Marker> driverList = {};
-  List laList = [];
-  List longList = [];
-
-  Set<Marker> onFeatchDriver() {
-    setState(() {
-      for (var i = 0; i < latitudeList.length; i++) {
-        markerList.add(Marker(
-          //add first marker
-          markerId: MarkerId(showLocation.toString()),
-          position:
-              LatLng(latitudeList[i]['latitude'], latitudeList[i]['longitude']),
-          //position of marker
-          infoWindow: const InfoWindow(
-            //popup info
-            title: 'Marker Title First ',
-            snippet: 'My Custom Subtitle',
-          ),
-          icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-        ));
-      }
-    });
-    return markerList;
-  }
-
-  displayDriver() {
-    DatabaseReference refs = FirebaseDatabase.instance.ref('Driver');
-    refs.onValue.listen((event) {
-      DataSnapshot latitude = event.snapshot;
-      Map la = latitude.value as Map;
-      la.forEach((key, value) {
-        setState(() {
-          latitudeList.add(value);
-          for (var i = 0; i < latitudeList.length; i++) {
-            onFeatchDriver();
-          }
-        });
-      });
-    });
-    setState(() {});
-  }
-
-  List<LatLng> latLen = [
-    // LatLng(19.0759837, 72.8776559),
-    // LatLng(28.679079, 77.069710),
-    // LatLng(26.850000, 80.949997),
-    // LatLng(24.879999, 74.629997),
-    // LatLng(16.166700, 74.833298),
-    // LatLng(12.971599, 77.594563),
-  ];
-
   var dasdasd;
-  List latitudeList = [];
   final Set<Marker> _markers = {};
+  LocationPermission? permission;
+  LatLng? _currentPosition;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    onDisplayDriver();
+  }
 
   onDisplayDriver() async {
     DatabaseReference refs = FirebaseDatabase.instance.ref('Driver');
-    // latLen.add(currentPostion);
     refs.onValue.listen((event) async {
-      DataSnapshot latitude = event.snapshot;
-      Map la = latitude.value as Map;
-      la.forEach((key, value) {
-        setState(() async {
-          latitudeList.add(value);
-          print(latitudeList);
-
-          // latLen.add(LatLng(latitudeList[i]['latitude'], latitudeList[i]['longitude']));
-          for (var i = 0; i < latitudeList.length; i++) {
-            latLen.add(LatLng(
-                latitudeList[i]['latitude'], latitudeList[i]['longitude']));
-            // driverList.add(Marker(
-            //   //add first marker
-            //   markerId: MarkerId(showLocation.toString()),
-            //   position: LatLng(
-            //       latitudeList[i]['latitude'], latitudeList[i]['longitude']),
-            //   //position of marker
-            //   infoWindow: const InfoWindow(
-            //     //popup info
-            //     title: 'Marker Title First ',
-            //     snippet: 'My Custom Subtitle',
-            //   ),
-            //   icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-            // ));
-            _markers.add(
-              // added markers
-              Marker(
-                markerId: MarkerId(i.toString()),
-                position: latLen[i],
-                infoWindow: InfoWindow(
-                  title: 'HOTEL',
-                  snippet: '5 Star Hotel',
-                ),
-                icon: BitmapDescriptor.defaultMarker,
-              ),
-            );
-            setState(() {});
-          }
-        });
+      _latLen.clear();
+      DataSnapshot location = event.snapshot;
+      Map drvData = location.value as Map;
+      drvData.forEach((key, value) async {
+        _latLen.add(LatLng(value['latitude'], value['longitude']));
+        loadData(_latLen);
       });
     });
-   }
-
-  Set<Marker> onGetDriver() {
-    setState(() {
-      markerList.add(Marker(
-        //add first marker
-        markerId: MarkerId(showLocation.toString()),
-        position: currentPostion, //position of marker
-        infoWindow: const InfoWindow(
-          //popup info
-          title: 'Marker Title First ',
-          snippet: 'My Custom Subtitle',
-        ),
-        icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-      ));
-      markerList.add(Marker(
-        //add first marker
-        markerId: MarkerId(showLocation.toString()),
-        position: currentPostion, //position of marker
-        infoWindow: const InfoWindow(
-          //popup info
-          title: 'Marker Title First ',
-          snippet: 'My Custom Subtitle',
-        ),
-        icon: BitmapDescriptor.defaultMarker, //Icon for Marker
-      ));
-    });
-
-    return markerList;
   }
-
-  final Completer<GoogleMapController> _controller = Completer();
-  bool isloading = false;
 
   void onMapCreated(GoogleMapController controller) {
     _controller.complete(controller);
   }
-
-  LocationPermission? permission;
-  LatLng? _currentPosition;
-  bool _isLoading = true;
 
   getLocation() async {
     permission = await Geolocator.requestPermission();
@@ -221,13 +65,26 @@ class _MapScreenState extends State<MapScreen> {
         desiredAccuracy: LocationAccuracy.high);
     double lat = position.latitude;
     double long = position.longitude;
-
     LatLng location = LatLng(lat, long);
-
     setState(() {
       _currentPosition = location;
       _isLoading = false;
     });
+  }
+
+  final List<LatLng> _latLen = <LatLng>[];
+
+  loadData(List<LatLng> listData) async {
+    for (int i = 0; i < listData.length; i++) {
+      _markers.add(Marker(
+        markerId: MarkerId(i.toString()),
+        position: listData[i],
+        infoWindow: InfoWindow(
+          title: 'Location: ' + i.toString(),
+        ),
+      ));
+      setState(() {});
+    }
   }
 
   @override
@@ -243,7 +100,7 @@ class _MapScreenState extends State<MapScreen> {
                     compassEnabled: true,
                     buildingsEnabled: true,
                     myLocationEnabled: true,
-                    markers: _markers,
+                    markers: Set<Marker>.of(_markers),
                     // markers: Set<Marker>.of(driverList),
                     onMapCreated: onMapCreated,
                     initialCameraPosition: CameraPosition(
@@ -278,22 +135,17 @@ class _MapScreenState extends State<MapScreen> {
                       ),
                       ElevatedButton(
                           onPressed: () {
+                            onDisplayDriver();
                             // udl.getLocation();
                           },
-                          child: Text('dad')),
+                          child: Text('Refesh')),
+                      Text('status request: (${_latLen.length})'),
                       Flexible(
                         child: ListView.builder(
                             padding: const EdgeInsets.all(8),
-                            itemCount: latitudeList.length,
+                            itemCount: _latLen.length,
                             itemBuilder: (BuildContext context, int index) {
-                              return Column(
-                                children: [
-                                  Text(
-                                      'Entry ${latitudeList[index]['latitude']}'),
-                                  Text(
-                                      'Entry ${latitudeList[index]['longitude']}'),
-                                ],
-                              );
+                              return Text('Entry (${_latLen[index]})');
                             }),
                       ),
                       Align(
@@ -358,7 +210,6 @@ class MapScreenController {
   final readStorage = FirebaseDatabase.instance;
   var readData = '';
   DatabaseReference refs = FirebaseDatabase.instance.ref("users/123");
-  Getter? gets = Getter();
   LatLng center = const LatLng(45.521563, -122.677433);
   late GoogleMapController mapController;
   var protect = false;
@@ -371,14 +222,6 @@ class MapScreenController {
 
   onRequestLocation() async {
     await refs.set({"name": "John", "age": 18, "address": "100 Mountain View"});
-  }
-
-  findDriver() {
-    refs.onValue.listen((DatabaseEvent event) {
-      gets?.age = int.parse(event.snapshot.child('age').value.toString());
-      readData = (gets?.age).toString();
-    });
-    addderewfg(readData);
   }
 
   Future<void> addUser() {
@@ -427,8 +270,4 @@ class MapScreenController {
   test() {}
 }
 
-class Getter {
-  String name = '';
-  int age = 0;
-  String note = '';
-}
+class FetchDriver {}
