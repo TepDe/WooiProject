d68362dad06169da53ec2d33f1834dcb3701959a
TepDe <unsokontep.swdev@gmail.com>
2023-02-28 17:01:51 +0700

    Update Themes.dart

--- lib/WidgetReUse/Themes.dart
+++ lib/WidgetReUse/Themes.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 class ThemesApp{
   var deepOrange = const Color(0xffffac30);
-  var liteOrange = const Color(0xfffff8eb);
+  var liteOrange = const Color(0xffffdda1);
+  var litestOrange = const Color(0xfffff3e0);
 
   //var yellow = Colors.yellow;
-  var orange =  Color(0xffffcb88);
+  // var orange = const Color(0xffffdfb4);
+  //var orange = Color.fromRGBO(250, 182, 0,1);
   var yellow = const Color(0xFFFFF243);
+  var orange = const Color(0xFFFAB600);
   var black = const Color(0xff000000);
   var grey = const Color(0xffa9a9a9);
+  var minGrey = const Color(0xffcecece);
+  var midGrey = const Color(0xffe1e1e1);
+  var green = const Color(0xff186a3b);
+  var red = const Color(0xffe74c3c);
+  var liteRed = const Color(0xfff1948a);
+  var liteGreen = const Color(0xff00cc71);
   var darkGrey = const Color(0xff616161);
-  var liteGrey = const Color(0xffeeeeee);
+  var liteGrey = const Color(0xfff8f8f8);
   var white = const Color(0xffffffff);
+  var liteBlue = const Color(0xffaed6f1);
+  var Blue = const Color(0xff2874a6);
 
 }
