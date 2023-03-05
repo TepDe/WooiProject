95adfea661bd0f5d1adc52a79304d8e37ae11b7f
TepDe <121762225+TepDe@users.noreply.github.com>
2023-03-05 22:07:26 +0700

    Update RenderListDetail.dart

--- lib/RenderListDetail.dart
+++ lib/RenderListDetail.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:wooiproject/WidgetReUse/ReUseWidget.dart';
 
+import 'WidgetReUse/Themes.dart';
+
 class RenderListDetail extends StatefulWidget {
   const RenderListDetail({Key? key}) : super(key: key);
 
@@ -13,26 +15,29 @@ class RenderListDetail extends StatefulWidget {
 class _RenderListDetailState extends State<RenderListDetail> {
   final reUse = ReUseWidget();
   var argumentData = Get.arguments;
+  final theme = ThemesApp();
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    if(argumentData == 0){
-      renderList =  reUse.reUseListPackage();
-    }else{
-      renderList =  reUse.ruTextBox();
+    if (argumentData == 0) {
+      renderList = reUse.reUseListPackage(removeFoot: true,Title: 'Total Package',headerColor: theme.dirt);
+    } else if (argumentData == 1) {
+      renderList = reUse.reUseListPackage(Title: 'Pending',headerColor: theme.orange);
+    } else if (argumentData == 2) {
+      renderList = reUse.reUseListPackage(Title: 'Complete',headerColor: theme.liteGreen);
+    } else if (argumentData == 3) {
+      renderList = reUse.reUseListPackage(Title: 'Return',headerColor: theme.liteRed);
     }
-
-
   }
-  var renderList ;
+
+  var renderList;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-          body: renderList),
+      child: Scaffold(body: renderList),
     );
   }
 }
