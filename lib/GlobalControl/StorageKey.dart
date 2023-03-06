fb9c4487316fe89c288bb93d5ced24e37f0cbc70
TepDe <121762225+TepDe@users.noreply.github.com>
2023-03-06 21:46:07 +0700

    Create StorageKey.dart

---
+++ lib/GlobalControl/StorageKey.dart
@@ -0,0 +1,15 @@
+class StorageKey {
+  String isGoOnline = 'isGoOnline';
+  String userName = 'userName';
+  String latitude = 'latitude';
+  String longitude = 'longitude';
+  String phoneNumber = 'phoneNumber';
+  String email = 'email';
+  String uid = 'uid';
+  String userID = 'userID';
+  String completePackage = 'completePackage';
+  String returnPackage = 'returnPackage';
+  String totalPackage = 'totalPackage';
+  String pendingPackage = 'pendingPackage';
+  String password = 'password';
+}
