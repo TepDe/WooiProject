4cfa6e325a921911b5f567d6306a26180365ab04
TepDe <121762225+TepDe@users.noreply.github.com>
2023-03-23 20:22:50 +0700

    Update CreatePackageScreen.dart

--- lib/CreatePackageScreen.dart
+++ lib/CreatePackageScreen.dart
@@ -21,7 +21,16 @@ class _CreatePackageScreenState extends State<CreatePackageScreen> {
   final phoneBox = TextEditingController();
   final locationBox = TextEditingController();
   final qtyBox = TextEditingController();
-  double textSize = 14;
+  final noteBox = TextEditingController();
+  double textSize = 12;
+  String packageID = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    packageID = glb.generatePackageID();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +38,6 @@ class _CreatePackageScreenState extends State<CreatePackageScreen> {
 
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Create Package'),
-        ),
         body: SingleChildScrollView(
           padding: EdgeInsets.only(bottom: 50),
           child: Expanded(
@@ -41,44 +47,83 @@ class _CreatePackageScreenState extends State<CreatePackageScreen> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  Row(
+                    children: [
+                      IconButton(
+                          splashRadius: 25,
+                          onPressed: () {
+                            Get.back();
+                          },
+                          icon: Icon(Icons.arrow_back)),
+                      Text(
+                        'Back',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: theme.black,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 18.0),
+                    padding: const EdgeInsets.only(left: 30, top: 25.0),
                     child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
+                        // reUse.reUseText(
+                        //     content: '    ID: ',
+                        //     size: textSize,
+                        //     color: theme.grey),
                         reUse.reUseText(
-                            content: 'Package ID: ',
-                            size: textSize,
-                            color: theme.grey),
+                            content: 'ID : ',
+                            size: 26.0,
+                            color: theme.black,
+                            weight: FontWeight.bold),
                         reUse.reUseText(
-                            content: 'PK00256',
-                            size: 20.0,
+                            content: packageID,
+                            size: 26.0,
                             color: theme.black,
                             weight: FontWeight.bold),
                       ],
                     ),
                   ),
+                  Divider(
+                    color: theme.grey,
+                    height: 1,
+                  ),
                   SizedBox(
                     height: 18,
                   ),
-                  reUse.reUseText(
-                      content: 'Receiver Phone Number:',
-                      size: textSize,
-                      color: theme.grey),
-                  reUse.reuseTextField(
-                      textIcon: Icons.phone,
-                      label: 'Receiver Phone Number',
-                      controller: phoneBox),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: reUse.reUseText(
+                        content: 'Receiver Phone Number:',
+                        size: textSize,
+                        color: theme.grey),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: reUse.reuseTextField(
+                        textIcon: Icons.phone,
+                        label: 'Receiver Phone Number',
+                        controller: phoneBox),
+                  ),
                   SizedBox(
                     height: 18,
                   ),
-                  reUse.reUseText(
-                      content: 'Receiver Location :',
-                      size: textSize,
-                      color: theme.grey),
-                  reUse.reuseTextField(
-                      controller: locationBox,
-                      label: 'Receiver Location',
-                      textIcon: Icons.location_on),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: reUse.reUseText(
+                        content: 'Receiver Location :',
+                        size: textSize,
+                        color: theme.grey),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: reUse.reuseTextField(
+                        controller: locationBox,
+                        label: 'Receiver Location',
+                        textIcon: Icons.location_on),
+                  ),
                   SizedBox(
                     height: 18,
                   ),
@@ -89,13 +134,14 @@ class _CreatePackageScreenState extends State<CreatePackageScreen> {
                       reUse.reUseCustomizeButton(
                           icon: Icons.remove,
                           showIcon: true,
-                          iconcolor: theme.white,
-                          colorBC: theme.hiLiteBlue,
+                          iconcolor: theme.orange,
+                          colorBC: theme.liteOrange,
                           isBcColor: true),
                       Container(
                         width: 60,
                         margin: const EdgeInsets.symmetric(horizontal: 30),
                         child: TextField(
+                          controller: qtyBox,
                           decoration: InputDecoration(
                             border: OutlineInputBorder(),
                             hintStyle: TextStyle(fontSize: 12),
@@ -106,8 +152,8 @@ class _CreatePackageScreenState extends State<CreatePackageScreen> {
                       reUse.reUseCustomizeButton(
                           icon: Icons.add,
                           showIcon: true,
-                          iconcolor: theme.white,
-                          colorBC: theme.hiLiteBlue,
+                          iconcolor: theme.orange,
+                          colorBC: theme.liteOrange,
                           isBcColor: true)
                     ],
                   ),
@@ -119,44 +165,88 @@ class _CreatePackageScreenState extends State<CreatePackageScreen> {
                   Container(
                     alignment: Alignment.center,
                     child: TextField(
+                      controller: noteBox,
                       keyboardType: TextInputType.multiline,
                       maxLines: 3,
                       decoration: InputDecoration(
                           border: OutlineInputBorder(
-                            borderSide:
-                                const BorderSide(color: Colors.grey, width: 0.0),
+                            borderSide: const BorderSide(
+                                color: Colors.grey, width: 0.0),
                           ),
 
                           // hintText: "Enter Remarks",
                           focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(width: 1, color: theme.hiLiteBlue))),
+                              borderSide: BorderSide(
+                                  width: 1, color: theme.hiLiteBlue))),
                     ),
+                  ),
+                  SizedBox(
+                    height: 18,
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Text('Cancel')),
-                      ElevatedButton(
-                          onPressed: () async {
-                            await glb
-                                .requestPackage(
-                                    qty: qtyBox.text.trim().toString(),
-                                    phoneNumber: phoneBox.text.trim().toString(),
-                                    location: locationBox.text.trim().toString())
-                                .then((value) {
-                              //qtyBox.clear();
-                              phoneBox.clear();
-                              locationBox.clear();
-                            });
+                      
+                      Flexible(
+                        child: Container(
+                          height: 40,
+                          width: Get.width,
+                          decoration: BoxDecoration(
+                            color: theme.orange,
+                            borderRadius: BorderRadius.circular(6),
+                            // boxShadow: [
+                            //   BoxShadow(
+                            //     color: Colors.grey,
+                            //     blurRadius: 1,
+                            //     //offset: Offset(4, 8), // Shadow position
+                            //   ),
+                            // ],
+                          ),
+                          child: Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              onTap: () async {
+                                FocusScope.of(context).unfocus();
+                                await glb
+                                    .requestPackage(
+                                        note: noteBox.text.trim().toString(),
+                                        packageID: packageID.toString(),
+                                        qty: qtyBox.text.trim().toString(),
+                                        phoneNumber:
+                                            phoneBox.text.trim().toString(),
+                                        location:
+                                            locationBox.text.trim().toString())
+                                    .then((value) {
+                                  //qtyBox.clear();
+                                  phoneBox.clear();
+                                  locationBox.clear();
+                                  qtyBox.clear();
+                                  noteBox.clear();
+                                });
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  // Padding(
+                                  //   padding: const EdgeInsets.only(right: 8.0),
+                                  //   child: Icon(
+                                  //     Icons,
+                                  //     color: iconcolor,
+                                  //   ),
+                                  // ),
 
-                            Navigator.pop(context);
-                          },
-                          child: Text('OK')),
+                                  Text(
+                                    'OK',
+                                    style: TextStyle(
+                                        color: theme.white,
+                                        fontWeight: FontWeight.normal),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
                     ],
                   ),
                 ],
