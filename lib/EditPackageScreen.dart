33d2e6eb4d070797de211db390b3ce87bb697f7b
TepDe <121762225+TepDe@users.noreply.github.com>
2023-03-26 17:41:09 +0700

    Create EditPackageScreen.dart

---
+++ lib/EditPackageScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditPackageScreen extends StatefulWidget {
+  const EditPackageScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditPackageScreen> createState() => _EditPackageScreenState();
+}
+
+class _EditPackageScreenState extends State<EditPackageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
