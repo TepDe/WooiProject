a834f2f03737af2bbfedfc28d987c336233a8ce1
TepDe <121762225+TepDe@users.noreply.github.com>
2023-03-29 22:28:49 +0700

    Update ReturnScreen.dart

--- lib/ReturnScreen.dart
+++ lib/ReturnScreen.dart
@@ -5,6 +5,7 @@ import 'package:get/get_core/src/get_main.dart';
 import 'package:wooiproject/GlobalControl/GlobalController.dart';
 import 'package:wooiproject/WidgetReUse/ReUseWidget.dart';
 import 'package:wooiproject/WidgetReUse/Themes.dart';
+
 class ReturnScreen extends StatefulWidget {
   const ReturnScreen({Key? key}) : super(key: key);
 
@@ -13,32 +14,47 @@ class ReturnScreen extends StatefulWidget {
 }
 
 class _ReturnScreenState extends State<ReturnScreen> {
-
   final reUse = ReUseWidget();
   final theme = ThemesApp();
   final glb = GlobalController();
   var argumentData = Get.arguments;
   List returnList = [];
   FirebaseAuth auth = FirebaseAuth.instance;
+  bool isShow = false;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     returnList = argumentData;
+    isShow = true;
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: Column(
-            children: [
-              reUse.reUseHeader(
-                  label: 'Return', title: 'Return', headercolor: theme.liteRed),
-              reUse.reTotalPackageListview(pkc: returnList)
-            ],
-          ),
-        ));
+      body: Column(
+        children: [
+          reUse.reUseHeader(
+              label: 'Return', title: 'Return', headercolor: theme.liteRed),
+          returnList.isEmpty
+              ? Flexible(
+                  flex: 3,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.notifications,
+                        color: theme.grey,
+                        size: 40,
+                      ),
+                      reUse.reUseText(content: 'No Return \n Available', color: theme.grey)
+                    ],
+                  ))
+              : reUse.reTotalPackageListview(pkc: returnList),
+        ],
+      ),
+    ));
   }
 }
