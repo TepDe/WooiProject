517c86637cffca42ad25a095870669fb747c3b38
TepDe <121762225+TepDe@users.noreply.github.com>
2023-03-29 22:29:03 +0700

    Update TotalPackageScreen.dart

--- lib/TotalPackageScreen.dart
+++ lib/TotalPackageScreen.dart
@@ -3,7 +3,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
-import 'package:wooiproject/GlobalControl/GlobalController.dart';
 
 import 'WidgetReUse/ReUseWidget.dart';
 import 'WidgetReUse/Themes.dart';
@@ -18,42 +17,44 @@ class TotalPackageScreen extends StatefulWidget {
 class _TotalPackageScreenState extends State<TotalPackageScreen> {
   final reUse = ReUseWidget();
   final theme = ThemesApp();
-  final glb = GlobalController();
   var argumentData = Get.arguments;
   List driverList = [];
   FirebaseAuth auth = FirebaseAuth.instance;
   bool isShow = false;
 
-  totalListLength() async {
-    DatabaseReference refs = FirebaseDatabase.instance
-        .ref('PackageRequest')
-        .child(auth.currentUser!.uid);
-    refs.onValue.listen((event) async {
-      driverList.clear();
-      setState(() {});
-      DataSnapshot driver = event.snapshot;
-      Map values = driver.value as Map;
-      values.forEach((key, values) {
-        print(values);
-        print(values);
-        Map data = values as Map;
-        data.forEach((key, value) {
-          setState(() {
-            driverList.add(value);
-          });
-        });
-      });
-    });
-  }
+  // totalListLength() async {
+  //   DatabaseReference refs = FirebaseDatabase.instance
+  //       .ref('PackageRequest')
+  //       .child(auth.currentUser!.uid);
+  //   refs.onValue.listen((event) async {
+  //     driverList.clear();
+  //     DataSnapshot driver = event.snapshot;
+  //     Map values = driver.value as Map;
+  //     values.forEach((key, values) {
+  //       Map data = values as Map;
+  //       data.forEach((key, value) async {
+  //         setState(() {
+  //           driverList.add(value);
+  //         });
+  //         isShow = true;
+  //       });
+  //     });
+  //   });
+  // }
 
   _TotalPackageScreenState() {
-    totalListLength();
+    driverList = argumentData;
+    isShow = true;
+
+    print(driverList);
+    print(driverList);
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    //totalListLength();
   }
 
   @override
@@ -71,7 +72,16 @@ class _TotalPackageScreenState extends State<TotalPackageScreen> {
                     label: 'Total Package',
                     title: 'Total Package',
                     headercolor: theme.dirt),
-                reUse.reTotalPackageListview(pkc: driverList),
+
+                isShow == false
+                    ? const Flexible(
+                        child: Center(
+                            child: SizedBox(
+                                height: 50,
+                                width: 50,
+                                child: CircularProgressIndicator())))
+                    : reUse.reTotalPackageListview(pkc: driverList),
+
                 // isShow == true
                 //     ? driverList == []
                 //         ? Flexible(child: Center(child: Text('No Package')))
