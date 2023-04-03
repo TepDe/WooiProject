5dfe4a7ca063985d48e024413d3b5e94f539884a
TepDe <unsokontep.swdev@gmail.com>
2023-04-03 17:29:06 +0700

    Update AccountScreen.dart

--- lib/AccountScreen.dart
+++ lib/AccountScreen.dart
@@ -9,6 +9,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wooiproject/GlobalControl/GlobalController.dart';
 import 'package:wooiproject/GlobalControl/StorageKey.dart';
 import 'package:wooiproject/LoginScreen.dart';
+import 'package:wooiproject/WidgetReUse/ReUseWidget.dart';
+import 'package:wooiproject/WidgetReUse/Themes.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({Key? key}) : super(key: key);
@@ -53,7 +55,6 @@ class _AccountScreenState extends State<AccountScreen> {
         .get()
         .then((DocumentSnapshot documentSnapshot) {
       getUserID = documentSnapshot['qty'];
-
     });
   }
 
@@ -66,10 +67,8 @@ class _AccountScreenState extends State<AccountScreen> {
     getPassword = prefs.getString(str.password) ?? '';
     getUserID = prefs.getString(str.userID) ?? '';
 
-    setState(() {
-    });
+    setState(() {});
   }
-
 
   fetchUserData() async {
     FirebaseAuth auth = FirebaseAuth.instance;
@@ -88,47 +87,195 @@ class _AccountScreenState extends State<AccountScreen> {
     setState(() {});
   }
 
+  final reUse = ReUseWidget();
+  final theme = ThemesApp();
+  bool light = false;
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    var imageSize = MediaQuery.of(context).size.height * 0.1;
+    var viewHeight = MediaQuery.of(context).size.height * 0.05;
+    var viewHeight2 = MediaQuery.of(context).size.height * 0.03;
+    var viewHeight3 = MediaQuery.of(context).size.height * 0.08;
+    var padding = MediaQuery.of(context).size.height * 0.01;
+    return
+        //   SafeArea(
+        //   child: Scaffold(
+        //     body: Column(
+        //       children: [
+        //         Text('AccountScreen'),
+        //         ElevatedButton(
+        //             onPressed: () {
+        //               setState(() async{
+        //                 await auth.signOut();
+        //                 Get.to(LogInScreen());
+        //               });
+        //             },
+        //             child: Text('Sign Out')),
+        //         Text('${driverList.length}'),
+        //         Text('${getLatitude ?? 'loading...'}'),
+        //         Text('${getLongitude ?? 'loading...'}'),
+        //         Text('${getUid ?? 'loading...'}'),
+        //         Text('${getEmail ?? 'loading...'}'),
+        //         Text('${getPassword ?? 'loading...'}'),
+        //         Text('${getUserID ?? 'loading...'}'),
+        //         ElevatedButton(
+        //           onPressed: () {
+        //             setState(() {
+        //
+        //               Fluttertoast.showToast(
+        //                 msg: 'Hello, world!',
+        //                 toastLength: Toast.LENGTH_SHORT,
+        //                 gravity: ToastGravity.BOTTOM,
+        //                 timeInSecForIosWeb: 1,
+        //                 backgroundColor: Colors.red,
+        //                 textColor: Colors.black,
+        //                 fontSize: 16.0,
+        //               );
+        //             });
+        //
+        //           },
+        //           child: Text('Show Toast'),
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // );
+        SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Text('AccountScreen'),
-            ElevatedButton(
-                onPressed: () {
-                  setState(() async{
-                    await auth.signOut();
-                    Get.to(LogInScreen());
-                  });
-                },
-                child: Text('Sign Out')),
-            Text('${driverList.length}'),
-            Text('${getLatitude ?? 'loading...'}'),
-            Text('${getLongitude ?? 'loading...'}'),
-            Text('${getUid ?? 'loading...'}'),
-            Text('${getEmail ?? 'loading...'}'),
-            Text('${getPassword ?? 'loading...'}'),
-            Text('${getUserID ?? 'loading...'}'),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-
-                  Fluttertoast.showToast(
-                    msg: 'Hello, world!',
-                    toastLength: Toast.LENGTH_SHORT,
-                    gravity: ToastGravity.BOTTOM,
-                    timeInSecForIosWeb: 1,
-                    backgroundColor: Colors.red,
-                    textColor: Colors.black,
-                    fontSize: 16.0,
-                  );
-                });
-
-              },
-              child: Text('Show Toast'),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: viewHeight2,
+                ),
+                reUse.reUseText(
+                    content: 'My profile', weight: FontWeight.bold, size: 15.0),
+                SizedBox(
+                  height: viewHeight2,
+                ),
+                Container(
+                  height: imageSize,
+                  width: imageSize,
+                  padding: const EdgeInsets.all(3.0),
+                  // decoration: BoxDecoration(
+                  //     borderRadius: BorderRadius.circular(50),
+                  //     border: Border.all(color: theme.orange, width: 1.5)),
+                  child: const CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSe1IKGF9z_2YNk4INs_zur1TjFIUtgpw_Ic2Jp2xxH5g&s"),
+                  ),
+                ),
+                SizedBox(
+                  height: viewHeight2,
+                ),
+                reUse.reUseText(
+                    content: 'John Wick',
+                    weight: FontWeight.bold,
+                    size: 20.0,
+                    color: theme.black),
+                reUse.reUseText(
+                    content: 'ID : ' + getUserID.toString() ?? 'loading...',
+                    weight: FontWeight.bold,
+                    size: 12.0,
+                    color: theme.grey),
+                SizedBox(
+                  height: viewHeight2,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(padding),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: reUse.reUseText(
+                        content: 'General',
+                        weight: FontWeight.bold,
+                        size: 12.0,
+                        color: theme.grey),
+                  ),
+                ),
+                InkWell(
+                    onTap: () {
+                      reUse.reUseCircleDialog(
+                          context: context,
+                          title: 'title',
+                          content: reUse.reUseTextFormField(),
+                          icon: Icons.password);
+                    },
+                    child: Material(
+                      color: Colors.transparent,
+                      child: reUse.reUseSettingItem(
+                          trailingIcon: Icon(Icons.visibility_off),
+                          title: Text('Pin Code'),
+                          context: context,
+                          leading: Icon(Icons.password)),
+                    )),
+                reUse.reUseSettingItem(
+                    trailingIcon: Switch(
+                      // This bool value toggles the switch.
+                      value: light,
+                      activeColor: theme.btnBlue,
+                      onChanged: (bool value) {
+                        // This is called when the user toggles the switch.
+                        setState(() {
+                          light = value;
+                        });
+                      },
+                    ),
+                    title: Text('Light Mode'),
+                    context: context,
+                    leading: Icon(Icons.sunny)),
+                Padding(
+                  padding: EdgeInsets.all(padding),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: reUse.reUseText(
+                        content: 'Account',
+                        weight: FontWeight.bold,
+                        size: 12.0,
+                        color: theme.grey),
+                  ),
+                ),
+                reUse.reUseSettingItem(
+                    trailingIcon: Text(
+                      '${getEmail ?? 'loading...'}',
+                      style: TextStyle(color: theme.grey),
+                    ),
+                    title: Text('Email'),
+                    context: context,
+                    leading: Icon(Icons.email)),
+                reUse.reUseSettingItem(
+                    trailingIcon: Text(
+                      '${getPassword ?? 'loading...'}',
+                      style: TextStyle(color: theme.grey),
+                    ),
+                    title: Text('Password'),
+                    context: context,
+                    leading: Icon(Icons.password_rounded)),
+                Container(
+                  margin: EdgeInsets.all(padding),
+                  width: Get.width,
+                  child: TextButton.icon(
+                    onPressed: () {
+                      reUse.reUseCircleDialog(
+                          context: context,
+                          function: 'logout',
+                          title: 'Log Out',
+                          content: Center(
+                              child: Text('Are you sure you want to log out?')),
+                          icon: Icons.login_outlined);
+                    },
+                    icon: Icon(Icons.login_rounded),
+                    label: Text('Log Out'),
+                    style: TextButton.styleFrom(
+                      backgroundColor: theme.litestRed,
+                      foregroundColor: theme.red,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
