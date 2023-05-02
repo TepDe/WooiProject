adf5e12afd4d7396a7fb6d49a6a3472d4a655492
TepDe <121762225+TepDe@users.noreply.github.com>
2023-05-02 21:34:42 +0700

    Create clsField.dart

---
+++ lib/GlobalControl/clsField.dart
@@ -0,0 +1,18 @@
+class FieldData {
+  String uid = 'uid';
+  String location = 'location';
+  String pushKey = 'pushKey';
+  String phoneNumber = 'phoneNumber';
+  String token = 'token';
+  String chatid = 'chatid';
+  String latitude = "latitude";
+  String longitude = "longitude";
+  String date = 'date';
+  String qty = 'qty';
+  String packageID = 'packageID';
+  String note = 'note';
+  String status = 'status';
+  String price = 'price';
+  String recLatitude = "recLatitude";
+  String recLongitude = "recLongitude";
+}
