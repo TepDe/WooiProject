121bb8d05dbef3deee61243dd390590bb5eaa7a1
TepDe <121762225+TepDe@users.noreply.github.com>
2023-05-11 22:16:21 +0700

    Update clsField.dart

--- lib/GlobalControl/clsField.dart
+++ lib/GlobalControl/clsField.dart
@@ -18,4 +18,33 @@ class FieldData {
   String dLastName = "dLastname";
   String dFirstName = "dFirstname";
   String dPhone = "dPhoneNumber";
+  String senderPhone = "senderPhone";
 }
+
+
+class FieldInfo {
+  String uid = 'uid';
+  String location = 'location';
+  String pushKey = 'pushKey';
+  String phoneNumber = 'phoneNumber';
+  String email = 'email';
+  String password = 'password';
+  String token = 'token';
+  String chatid = 'chatid';
+  String userID = 'userID';
+  String latitude = "latitude";
+  String longitude = "longitude";
+  String date = 'date';
+  String qty = 'qty';
+  String packageID = 'packageID';
+  String note = 'note';
+  String status = 'status';
+  String price = 'price';
+  String firstName = 'firstName';
+  String lastName = 'lastName';
+  String recLatitude = "recLatitude";
+  String recLongitude = "recLongitude";
+  String returnDate = "returnDate";
+  String returnNote = "returnNote";
+}
+
