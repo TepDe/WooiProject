fe1ac2905f0ac6426253e4a1a6b0287fac6d3ca0
TepDe <121762225+TepDe@users.noreply.github.com>
2023-05-18 20:28:22 +0700

    Create language.dart

---
+++ lib/Distination/language.dart
@@ -0,0 +1,11 @@
+class ClsLanguage{
+  String totalPackage = 'កញ្ចប់សរុប';
+  String pending = 'កំពុងដំណើរការ';
+  String complete = 'បញ្ចប់';
+  String returns = 'ត្រឡប់';
+  String create = 'បង្កើត';
+  String home = 'ផ្ទាំងដើម';
+  String price = 'តំលៃទំនិញ';
+  String qty = 'ចំនួន';
+  String packageID = 'លេខកញ្ចប់';
+}
