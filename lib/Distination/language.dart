58151d84469c0e4e93e4aeb070fbccf9c47cca65
TepDe <unsokontep.swdev@gmail.com>
2023-05-25 16:58:48 +0700

    Update language.dart

--- lib/Distination/language.dart
+++ lib/Distination/language.dart
@@ -1,5 +1,5 @@
 class ClsLanguage{
-  String totalPackage = 'កញ្ចប់សរុប';
+  String totalPackage = 'ទំនិញសរុប';
   String pending = 'កំពុងដំណើរការ';
   String complete = 'បញ្ចប់';
   String returns = 'ត្រឡប់';
