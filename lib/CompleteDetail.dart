bb95ecad40b5af532fd7c5ecb3fd5f6cfa751544
TepDe <unsokontep.swdev@gmail.com>
2023-05-30 15:26:15 +0700

    Create CompleteDetail.dart

---
+++ lib/CompleteDetail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompleteDetail extends StatefulWidget {
+  const CompleteDetail({Key? key}) : super(key: key);
+
+  @override
+  State<CompleteDetail> createState() => _CompleteDetailState();
+}
+
+class _CompleteDetailState extends State<CompleteDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
