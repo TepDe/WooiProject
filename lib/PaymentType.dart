9c9181b9165ec0473f0cd569ebeebe6408f76af0
TepDe <unsokontep.swdev@gmail.com>
2023-07-03 10:53:29 +0700

    Create PaymentType.dart

---
+++ lib/PaymentType.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PaymentType extends StatefulWidget {
+  const PaymentType({Key? key}) : super(key: key);
+
+  @override
+  State<PaymentType> createState() => _PaymentTypeState();
+}
+
+class _PaymentTypeState extends State<PaymentType> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
