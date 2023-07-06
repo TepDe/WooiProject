127898ad49f0206b623666bf66eec933733649ff
TepDe <unsokontep.swdev@gmail.com>
2023-07-06 13:12:40 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,59 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:wooiproject/GlobalControl/GlobalController.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:wooiproject/LoginScreen.dart';
-import 'package:wooiproject/SetUpScreen.dart';
 import 'package:wooiproject/ViewScreen.dart';
+import 'dart:async';
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.notification?.body}');
+  // Add your custom logic to handle the background message here
+}
+FirebaseMessaging messaging = FirebaseMessaging.instance;
 
+void requestNotificationPermission() async {
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  print('Notification permission granted: ${settings.authorizationStatus}');
+}
+void configureFirebaseMessaging() {
+  FirebaseMessaging.onBackgroundMessage(_handleBackgroundMessage);
+  FirebaseMessaging.onMessage.listen(_handleMessage);
+  FirebaseMessaging.onMessageOpenedApp.listen(_handleMessageOpenedApp);
+}
+
+Future<void> _handleBackgroundMessage(RemoteMessage message) async {
+  print('Received background message: ${message.notification?.title}');
+  // Handle the message in the background
+}
+
+void _handleMessage(RemoteMessage message) {
+  print('Received message: ${message.notification?.title}');
+  // Handle the message when the app is in the foreground
+}
+
+void _handleMessageOpenedApp(RemoteMessage message) {
+  print('Opened app from notification: ${message.notification?.title}');
+  // Handle the message when the app is opened from a notification
+}
 void main() async {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  AndroidInitializationSettings androidInitializationSettings = const AndroidInitializationSettings('app_icon');
+  InitializationSettings initializationSettings = InitializationSettings(android: androidInitializationSettings);
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  requestNotificationPermission();
+  configureFirebaseMessaging();
   await Firebase.initializeApp();
   // await Firebase.initializeApp(
   //     options: const FirebaseOptions(
@@ -19,7 +63,7 @@ void main() async {
   //         appId: '1:1093614007557:android:9efb550fcadd369394f36c',
   //         messagingSenderId: '1093614007557',
   //         projectId: 'wooi-c715e'));
-
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler );
   runApp(GetMaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
