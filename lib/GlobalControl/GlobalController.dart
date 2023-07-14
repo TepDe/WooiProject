23440ac785730cfa622458b1cbeba4c961bdbdf1
TepDe <unsokontep.swdev@gmail.com>
2023-07-14 15:45:30 +0700

    Update GlobalController.dart

--- lib/GlobalControl/GlobalController.dart
+++ lib/GlobalControl/GlobalController.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 
