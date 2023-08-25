f19fe3fed1affa9618b0ab4691f1e2977e2b6c78
TepDe <121762225+TepDe@users.noreply.github.com>
2023-08-25 20:42:38 +0700

    Update SetUpScreen.dart

--- lib/SetUpScreen.dart
+++ lib/SetUpScreen.dart
@@ -254,156 +254,163 @@ class _SetUpScreenState extends State<SetUpScreen> {
                             height: Get.height * 0.03,
                           ),
                           Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            mainAxisSize: MainAxisSize.min,
                             children: [
-                              TextButton(
-                                  // style: ElevatedButton.styleFrom(
-                                  //   elevation: 1,
-                                  //   backgroundColor: Colors.transparent,
-                                  //   shape: RoundedRectangleBorder(
-                                  //     borderRadius: BorderRadius.circular(6),
-                                  //   ),
-                                  // ),
-                                  onPressed: () async {
-                                    await FirebaseAuth.instance.signOut().then(
-                                        (value) =>
-                                            Get.to(() => const LogInScreen()));
-                                  },
-                                  child: reUse.reUseText(content: "Log Out")),
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  elevation: 1,
-                                  backgroundColor: theme.orange,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(6),
-                                  ),
-                                ),
-                                onPressed: () async {
-                                  if (firstName.text.trim().toString() == '') {
-                                    await reUse.reUseCircleDialog(
-                                        context: context,
-                                        icon: Icons.account_circle_rounded,
-                                        title: 'Error',
-                                        content: Center(
-                                          child: Text(
-                                            'First name Must Include',
-                                            style: TextStyle(
-                                              color: theme.black,
-                                            ),
-                                          ),
-                                        ));
-                                  } else if (lastName.text.trim().toString() ==
-                                      '') {
-                                    await reUse.reUseCircleDialog(
-                                        context: context,
-                                        icon: Icons.account_circle_rounded,
-                                        title: 'Error',
-                                        content: Center(
-                                          child: Text(
-                                            'Last Name Must Include',
-                                            style: TextStyle(
-                                              color: theme.black,
-                                            ),
-                                          ),
-                                        ));
-                                  } else if (phoneNumber.text
-                                          .trim()
-                                          .toString() ==
-                                      '') {
-                                    await reUse.reUseCircleDialog(
-                                        context: context,
-                                        icon: Icons.phone_rounded,
-                                        title: 'Error',
-                                        content: Center(
-                                          child: Text(
-                                            'Phone Number Must Include',
-                                            style: TextStyle(
-                                              color: theme.black,
-                                            ),
-                                          ),
-                                        ));
-                                  } else if (bankName == '') {
-                                    await reUse.reUseCircleDialog(
-                                        context: context,
-                                        icon: Icons.monetization_on,
-                                        title: 'Error',
-                                        content: Center(
-                                          child: Text(
-                                            'Phone choose bank name',
-                                            style: TextStyle(
-                                              color: theme.black,
-                                            ),
-                                          ),
-                                        ));
-                                  } else if (lastName.text.isEmpty ||
-                                      firstName.text.isEmpty ||
-                                      phoneNumber.text.isEmpty ||
-                                      bankCode.text.isEmpty) {
-                                    await reUse.reUseCircleDialog(
-                                        context: context,
-                                        icon: Icons.close_rounded,
-                                        title: clsLan.remainEmpty,
-                                        function: '',
-                                        content: Center(
-                                          child: Text(
-                                            clsLan.emptyFill,
-                                            style: TextStyle(
-                                              color: theme.black,
-                                            ),
-                                          ),
-                                        ));
-                                  } else {
-                                    showDialog(
-                                      barrierDismissible: false,
-                                      context: context,
-                                      builder: (BuildContext context) {
-                                        return WillPopScope(
-                                          onWillPop: () async => false,
-                                          child: const AlertDialog(
-                                            elevation: 0,
-                                            backgroundColor: Colors.transparent,
-                                            actions: [
-                                              Center(
-                                                child: SizedBox(
-                                                    height: 40,
-                                                    width: 40,
-                                                    child:
-                                                        CircularProgressIndicator()),
-                                              )
-                                            ],
-                                          ),
-                                        );
+                              Flexible(
+                                child: SizedBox(
+                                  width: Get.width,
+                                  child: TextButton(
+                                      // style: ElevatedButton.styleFrom(
+                                      //   elevation: 1,
+                                      //   backgroundColor: Colors.transparent,
+                                      //   shape: RoundedRectangleBorder(
+                                      //     borderRadius: BorderRadius.circular(6),
+                                      //   ),
+                                      // ),
+                                      onPressed: () async {
+                                        await FirebaseAuth.instance.signOut().then(
+                                            (value) =>
+                                                Get.to(() => const LogInScreen()));
                                       },
-                                    );
+                                      child: reUse.reUseText(content: "Log Out")),
+                                ),
+                              ),
 
-                                    glb.storeSetUpAccount(
-                                        bankCode: bankCode.text.trim(),
-                                        bankName: bankName.trim(),
-                                        phoneNumber: phoneNumber.text.trim(),
-                                        firstname: firstName.text.trim(),
-                                        lastname: lastName.text.trim());
-                                  }
-
-                                  // await glb.setUpInfor(
-                                  //     phoneNumber: phoneNumber.text.trim(),
-                                  //     firstname: firstName.text.trim(),
-                                  //     lastname: lastName.text.trim());
-                                  // Get.to(const ViewScreen());
-
-                                  setState(() {});
-                                },
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Text(
-                                      clsLan.continues,
-                                      style: TextStyle(
-                                          color: theme.white,
-                                          fontWeight: FontWeight.w700),
+                              Flexible(
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    elevation: 1,
+                                    backgroundColor: theme.orange,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(6),
                                     ),
-                                    const Icon(Icons.navigate_next_rounded),
-                                  ],
+                                  ),
+                                  onPressed: () async {
+                                    if (firstName.text.trim().toString() == '') {
+                                      await reUse.reUseCircleDialog(
+                                          context: context,
+                                          icon: Icons.account_circle_rounded,
+                                          title: 'Error',
+                                          content: Center(
+                                            child: Text(
+                                              'First name Must Include',
+                                              style: TextStyle(
+                                                color: theme.black,
+                                              ),
+                                            ),
+                                          ));
+                                    } else if (lastName.text.trim().toString() ==
+                                        '') {
+                                      await reUse.reUseCircleDialog(
+                                          context: context,
+                                          icon: Icons.account_circle_rounded,
+                                          title: 'Error',
+                                          content: Center(
+                                            child: Text(
+                                              'Last Name Must Include',
+                                              style: TextStyle(
+                                                color: theme.black,
+                                              ),
+                                            ),
+                                          ));
+                                    } else if (phoneNumber.text
+                                            .trim()
+                                            .toString() ==
+                                        '') {
+                                      await reUse.reUseCircleDialog(
+                                          context: context,
+                                          icon: Icons.phone_rounded,
+                                          title: 'Error',
+                                          content: Center(
+                                            child: Text(
+                                              'Phone Number Must Include',
+                                              style: TextStyle(
+                                                color: theme.black,
+                                              ),
+                                            ),
+                                          ));
+                                    } else if (bankName == '') {
+                                      await reUse.reUseCircleDialog(
+                                          context: context,
+                                          icon: Icons.monetization_on,
+                                          title: 'Error',
+                                          content: Center(
+                                            child: Text(
+                                              'Phone choose bank name',
+                                              style: TextStyle(
+                                                color: theme.black,
+                                              ),
+                                            ),
+                                          ));
+                                    } else if (lastName.text.isEmpty ||
+                                        firstName.text.isEmpty ||
+                                        phoneNumber.text.isEmpty ||
+                                        bankCode.text.isEmpty) {
+                                      await reUse.reUseCircleDialog(
+                                          context: context,
+                                          icon: Icons.close_rounded,
+                                          title: clsLan.remainEmpty,
+                                          function: '',
+                                          content: Center(
+                                            child: Text(
+                                              clsLan.emptyFill,
+                                              style: TextStyle(
+                                                color: theme.black,
+                                              ),
+                                            ),
+                                          ));
+                                    } else {
+                                      showDialog(
+                                        barrierDismissible: false,
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return WillPopScope(
+                                            onWillPop: () async => false,
+                                            child: const AlertDialog(
+                                              elevation: 0,
+                                              backgroundColor: Colors.transparent,
+                                              actions: [
+                                                Center(
+                                                  child: SizedBox(
+                                                      height: 40,
+                                                      width: 40,
+                                                      child:
+                                                          CircularProgressIndicator()),
+                                                )
+                                              ],
+                                            ),
+                                          );
+                                        },
+                                      );
+
+                                      glb.storeSetUpAccount(
+                                          bankCode: bankCode.text.trim(),
+                                          bankName: bankName.trim(),
+                                          phoneNumber: phoneNumber.text.trim(),
+                                          firstname: firstName.text.trim(),
+                                          lastname: lastName.text.trim());
+                                    }
+
+                                    // await glb.setUpInfor(
+                                    //     phoneNumber: phoneNumber.text.trim(),
+                                    //     firstname: firstName.text.trim(),
+                                    //     lastname: lastName.text.trim());
+                                    // Get.to(const ViewScreen());
+
+                                    setState(() {});
+                                  },
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        clsLan.continues,
+                                        style: TextStyle(
+                                            color: theme.white,
+                                            fontWeight: FontWeight.w700),
+                                      ),
+                                      const Icon(Icons.navigate_next_rounded),
+                                    ],
+                                  ),
                                 ),
                               ),
                             ],
