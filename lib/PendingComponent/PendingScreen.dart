dbfc9276e3ca50db09208ed54fb51b235fbff752
TepDe <121762225+TepDe@users.noreply.github.com>
2023-11-10 00:45:18 +0700

    Update PendingScreen.dart

--- lib/PendingComponent/PendingScreen.dart
+++ lib/PendingComponent/PendingScreen.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wooiproject/Distination/language.dart';
@@ -10,7 +9,7 @@ import 'package:wooiproject/WidgetReUse/ReUseWidget.dart';
 import 'package:wooiproject/WidgetReUse/Themes.dart';
 
 class PendingScreen extends StatefulWidget {
-  const PendingScreen({Key? key}) : super(key: key);
+  const PendingScreen({super.key});
 
   @override
   State<PendingScreen> createState() => _PendingScreenState();
